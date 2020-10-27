1c03d0e04f41ef3ce78b2d69f43e17925843350e
Samj0ns0n <jonathanbaskar@gmail.com>
2020-10-27 10:28:00 +0100

    Changement des variables regex pattern en variable constantes comme le preconise PEP 8 + simplification de la loop

--- pattern_matching.py
+++ pattern_matching.py
@@ -11,18 +11,18 @@ NUMERIC = "NUMERIC"
 GEOGRAPHY = "GEOGRAPHY"
 STRING = "STRING"
 
-pattern_float = '[+-]?([0-9]*[.])?[0-9]+'
-pattern_string = '[a-z]'
-pattern_non_number = '[^a-zA-Z0-9]'
-pattern_date = r'^(\d{4})-(0[1-9]|1[0-2]|[1-9])-([1-9]|0[1-9]|[1-2]\d|3[0-1])'
-pattern_time = '^(0[1-9]|1[0-9]|2[0-3]):(0[0-9]|[0-5][0-9]):(0[0-9]|[0-5][0-9])'
-pattern_date_time =  r'^(\d{4})-(0[1-9]|1[0-2]|[1-9])-([1-9]|0[1-9]|[1-2]\d|3[0-1]) (0[1-9]|1[0-9]|2[0-3]):(0[0-9]|[0-5][0-9]):(0[0-9]|[0-5][0-9]).(\d{6}) (UTC)'
-pattern_bool = '(VRAI|vrai|Vrai|FAUX|Faux|faux|OUI|Oui|oui|NON|Non|non|YES|Yes|yes|NO|No|no|TRUE|True|true|FALSE|False|false)'
+PATTERN_FLOAT = '[+-]?([0-9]*[.])?[0-9]+'
+PATTERN_STRING = '[a-z]'
+PATTERN_NON_NUMBER = '[^a-zA-Z0-9]'
+PATTERN_DATE = r'^(\d{4})-(0[1-9]|1[0-2]|[1-9])-([1-9]|0[1-9]|[1-2]\d|3[0-1])'
+PATTERN_TIME = '^(0[1-9]|1[0-9]|2[0-3]):(0[0-9]|[0-5][0-9]):(0[0-9]|[0-5][0-9])'
+PATTERN_DATETIME =  r'^(\d{4})-(0[1-9]|1[0-2]|[1-9])-([1-9]|0[1-9]|[1-2]\d|3[0-1]) (0[1-9]|1[0-9]|2[0-3]):(0[0-9]|[0-5][0-9]):(0[0-9]|[0-5][0-9]).(\d{6}) (UTC)'
+PATTERN_BOOL = '(VRAI|vrai|Vrai|FAUX|Faux|faux|OUI|Oui|oui|NON|Non|non|YES|Yes|yes|NO|No|no|TRUE|True|true|FALSE|False|false)'
 
 def find_pattern(date, pattern):
-    
-    c_true = 0
-    
+
+    c_true = 0 
+
     if date.dtypes == 'object': 
         date_test = date.str.contains(pattern, regex = True, na=True)
         c_true = date_test.astype(str).tolist().count('True')
@@ -32,11 +32,12 @@ def find_pattern(date, pattern):
     else:
         return False
 
+
 def pattern_match(tab_types, tab_column, dataframe):
-
     # pattern matching
-
-    '''
+	
+	
+	'''
     print("type de tab_column ",type(tab_column))
     print("type de tab_type ",type(tab_types))
     print("type de dataframe ",type(dataframe))
@@ -44,32 +45,32 @@ def pattern_match(tab_types, tab_column, dataframe):
     print("tab_column dans pattern_matching = ",tab_column)
     print("tab_type dans pattern_matching = ",tab_types)
     print("dataframe dans pattern_matching = ",dataframe)
-    '''
+	'''
+	
+    for elements in tab_column:
 
-    for i in range(len(dataframe.columns)):
+        # print("pattern  = ",dataframe[elements].values)
 
-        # print("pattern  = ",dataframe[tab_column[i]].values)
+        date = find_pattern(dataframe[elements],PATTERN_DATE)
+        time = find_pattern(dataframe[elements], PATTERN_TIME)
+        datetime = find_pattern(dataframe[elements], PATTERN_DATETIME)
+        floating = find_pattern(dataframe[elements], PATTERN_FLOAT)
+        boolean = find_pattern(dataframe[elements], PATTERN_BOOL)
 
-        date = find_pattern(dataframe[tab_column[i]], pattern_date)
-        time = find_pattern(dataframe[tab_column[i]], pattern_time)
-        datetime = find_pattern(dataframe[tab_column[i]], pattern_date_time)
-        floating = find_pattern(dataframe[tab_column[i]], pattern_float)
-        boolean = find_pattern(dataframe[tab_column[i]], pattern_bool)
-
-        if datetime or dataframe[tab_column[i]].dtypes == 'datetime64[ns]' :
-            tab_types[i] = DATETIME
+        if datetime or dataframe[elements].dtypes == 'datetime64[ns]' :
+            tab_types[elements] = DATETIME
         elif time:
-            tab_types[i] = TIME
+            tab_types[elements] = TIME
         elif date:
-            tab_types[i] = DATE
-        elif floating or dataframe[tab_column[i]].dtypes == 'float64':
-            tab_types[i] = FLOAT
-        elif dataframe[tab_column[i]].dtypes == 'Int64':
-            tab_types[i] = INTEGER
-        elif boolean or dataframe[tab_column[i]].dtypes == 'bool':
-            tab_types[i] = BOOLEAN
+            tab_types[elements] = DATE
+        elif floating or dataframe[elements].dtypes == 'float64':
+            tab_types[elements] = FLOAT
+        elif dataframe[elements].dtypes == 'Int64':
+            tab_types[elements] = INTEGER
+        elif boolean or dataframe[elements].dtypes == 'bool':
+            tab_types[elements] = BOOLEAN
         else:
-            tab_types[i] = STRING
-
-    # print("Table des colonnes apres pattern matching : "tab_types)
+            tab_types[elements] = STRING
+    # print("Table des colonnes apres pattern matching : ", tab_types)
+	
     return tab_types
