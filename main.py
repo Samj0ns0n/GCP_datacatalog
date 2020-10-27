54e1e49b0320bfc0006afb3dae984264d2416950
Samj0ns0n <jonathanbaskar@gmail.com>
2020-10-27 11:57:41 +0100

    A cause des changements dans le fichier Read_write, il fallait modifier le main en conséquence

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import Read_write as rw
+from Read_write import selection
 from pattern_matching import pattern_match
 from action import actions
 
@@ -9,10 +9,11 @@ if __name__ == "__main__":
     # Vous serez devant la page du bucket que vous voulez
     # En face de vous il y aura le nom du bucket
     
-    #bucket_name = input("Selectionner le nom du bucket : ")
-    #path_to_csv = rw.selection(bucket_name)
-    #blob_metadata(bucket_name, path_to_csv)
-    actions("test-datapod","Test_header_virgule.csv")
+    # bucket_name = input("Selectionner le nom du bucket : ")
+    bucket_name = 'test-datapod'
+    csv_file = selection(bucket_name)
+    actions(bucket_name,csv_file[0])
+    # actions("test-datapod","Test_header_virgule.csv")
 
 
   
